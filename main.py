8ae6e40e6c0d6f202b6f41f6ff0c4999753548bb
Pedro <prian5667@gmail.com>
2025-01-16 21:22:57 -0300

    segunda, tentativas e dicas

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 alvo = 42
-try:
-    chute = int(input("Digite um numero inteiro entre 0 e 100: "))
-    if chute == alvo:
-        print("parabens, voce acertou")
-    else:
-        print("que pena, o numera era " + str(alvo))
-
-except:
-    print("numero invalido")
+quantidade_tentativas = 5
+jogador_acertou = False
+while quantidade_tentativas > 0 and not jogador_acertou :
+    try:
+        chute = int(input("Digite um numero inteiro entre 0 e 100: "))
+        if chute == alvo:
+            print("parabens, voce acertou")
+            jogador_acertou = True
+        elif chute > alvo:
+            print("errou o numero é menor")
+            quantidade_tentativas -= 1
+        else: 
+             print("errou o numero é maior")
+             quantidade_tentativas += 1
+            
+    except:
+        print("numero invalido")
+if not jogador_acertou:
+     print("que pena, o numera era " + str(alvo))
