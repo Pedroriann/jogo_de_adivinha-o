ac7562c33bac01e4d98445c59f8e60787808336f
Pedro Rian <115435151+Pedroriann@users.noreply.github.com>
2025-01-28 20:30:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-alvo = 42
-quantidade_tentativas = 5
-jogador_acertou = False
-while quantidade_tentativas > 0 and not jogador_acertou :
-    try:
-        chute = int(input("Digite um numero inteiro entre 0 e 100: "))
-        if chute == alvo:
-            print("parabens, voce acertou")
-            jogador_acertou = True
-        elif chute > alvo:
-            print("errou o numero é menor")
-            quantidade_tentativas -= 1
-        else: 
-             print("errou o numero é maior")
-             quantidade_tentativas += 1
-            
-    except:
-        print("numero invalido")
-if not jogador_acertou:
-     print("que pena, o numera era " + str(alvo))
+def adivinhe_o_numero():
+    print("Você escolheu Adivinhe o Número!")
+    import random
+    numero = random.randint(1, 10)
+    while True:
+        palpite = int(input("Tente adivinhar o número (entre 1 e 10): "))
+        if palpite == numero:
+            print("Parabéns! Você acertou!")
+            break
+        elif palpite < numero:
+            print("Tente um número maior.")
+        else:
+            print("Tente um número menor.")
